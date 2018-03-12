3ebf68672c02880d32610e7d0418f3f7f039cc35
sciuridae0603 <sciuridae0603@gmail.com>
2018-03-12 20:34:58 +0800

    add background

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+    $('#fullpage').fullpage({
+		navigation : true,
+		navigationPosition : "right",
+		slidesNavigation: true,
+		controlArrows: true,
+		verticalCentered: true,
+		verticalCentered: true,
+		easing: 'easeOutQuart',
+		easingcss3: 'cubic-bezier(0.860, 0.000, 0.070, 1.000)'
+    });     
+    
+    var tmp = document.getElementsByClassName("backgroundBlur")
+
+	for (let i = 0; i < tmp.length; i++) {
+		tmp[i].src = "/assets/bg/" + Math.floor(Math.random() * Math.floor(16 - 1) + 1) + ".jpg";
+    }	 
+    
+});
